f91d45bc38271d29f5a07206221c997f01ad7a58
firstof9 <firstof9@gmail.com>
2021-07-28 15:00:27 -0700

    Update const.py

--- custom_components/openevse/const.py
+++ custom_components/openevse/const.py
@@ -3,7 +3,7 @@ from homeassistant.const import (
     ENERGY_KILO_WATT_HOUR,
     TEMP_CELSIUS,
     TIME_MINUTES,
-    VOLT,
+    ELECTRIC_POTENTIAL_VOLT,
 )
 
 CONF_NAME = "name"
@@ -48,7 +48,12 @@ SENSOR_TYPES = {
         "mdi:package-up",
     ],
     "ambient_threshold": ["Ambient Threshold", TEMP_CELSIUS, "ambient_threshold", None],
-    "charging_voltage": ["Charging Voltage", VOLT, "charging_voltage", "mdi:sine-wave"],
+    "charging_voltage": [
+        "Charging Voltage",
+        ELECTRIC_POTENTIAL_VOLT,
+        "charging_voltage",
+        "mdi:sine-wave",
+    ],
     "charge_limit": [
         "Charge Limit",
         ENERGY_KILO_WATT_HOUR,
