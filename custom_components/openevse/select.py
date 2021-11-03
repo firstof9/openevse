37344e702faaa57ec98882dda5413ee54122b257
Chris <firstof9@gmail.com>
2021-11-03 09:19:21 -0700

    fix: updates from dev (#9)
    * Change library
    
    * Change from openevsewifi to openevsehttp
    * Remove sensors that are not yet ready
    
    * round the charge_time
    
    * Adjust charging_current sensor
    
    * typo
    
    * Add esp_temperature sensor and wifi signal sensor
    
    * Library bump
    
    * Create hacs.json
    
    * Add ConfigEntryNotReady if unable to get data on startup
    
    * more typing
    
    * Changes for 2021.9.x
    
    * more changes for 2021.9.x
    
    * Library bump
    
    * fix typo
    
    * add more debug messages
    
    * fix: amps selection
    
    * fixes #1
    
    * chore: add github work flows (#2)
    
    * fix: update state class on energy session sensor (#3)
    
    * chore: add github work flows
    
    * Update release-drafter.yml
    
    * fix: update state class on energy session sensor
    
    * fix: adjust calculated watts (#5)
    
    * chore: add github work flows
    
    * Update release-drafter.yml
    
    * Create README and CONTRIBUTING
    
    * Create LICENSE
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * fix: adjust calculated watts
    
    * Adjust calculated watts based on scale from OpenEVSE unit
    * Adds scale sensor
    
    * fix: merge fixes from beta into dev (#8)
    
    * chore: add github work flows
    
    * Update release-drafter.yml
    
    * Create README and CONTRIBUTING
    
    * Create LICENSE
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * fix: adjust calculated watts
    
    * Adjust calculated watts based on scale from OpenEVSE unit
    * Adds scale sensor
    
    * refactor: use SensorEntityDescription for sensors
    
    * fix: fix some incorrect classes
    
    * Remove deprecated properties
    
    * adjust some entity catagories
    
    * Revert charge time sensor
    
    * fix: select for max amps and service level not working
    
    * update send_command function
    * library dependency update
    *
    
    * utilize websockets for sensor updates (#7)
    
    * add more sensors
    * utilize EntityDescription class
    
    * refactor: clean up switch platform
    
    * fix binary sensor updates
    
    * properly fix power sensor calculation
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * add debug message to sensor updates
    
    * Update README.md
    
    * dependency bump
    
    * Update README.md

--- custom_components/openevse/select.py
+++ custom_components/openevse/select.py
@@ -1,14 +1,21 @@
+"""Support for OpenEVSE controls using the select platform."""
+from __future__ import annotations
 import logging
 from typing import Any, Optional
 
 from homeassistant.components.select import SelectEntity
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.const import CONF_NAME
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
-from requests import RequestException
 
-from . import connect, send_command
-from .const import COORDINATOR, DOMAIN, SELECT_TYPES
+from . import (
+    send_command,
+    CommandFailed,
+    InvalidValue,
+    OpenEVSEManager,
+    OpenEVSEUpdateCoordinator,
+)
+from .const import COORDINATOR, DOMAIN, MANAGER, SELECT_TYPES
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -16,9 +23,10 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(hass, entry, async_add_entities):
     """Set up the OpenEVSE selects."""
     coordinator = hass.data[DOMAIN][entry.entry_id][COORDINATOR]
+    manager = hass.data[DOMAIN][entry.entry_id][MANAGER]
     selects = []
     for select in SELECT_TYPES:
-        selects.append(OpenEVSESelect(hass, entry, coordinator, select))
+        selects.append(OpenEVSESelect(hass, entry, coordinator, select, manager))
 
     async_add_entities(selects, False)
 
@@ -26,7 +34,14 @@ async def async_setup_entry(hass, entry, async_add_entities):
 class OpenEVSESelect(CoordinatorEntity, SelectEntity):
     """Define OpenEVSE Service Level select."""
 
-    def __init__(self, hass, config_entry: ConfigEntry, coordinator, name: str) -> None:
+    def __init__(
+        self,
+        hass,
+        config_entry: ConfigEntry,
+        coordinator: OpenEVSEUpdateCoordinator,
+        name: str,
+        manager: OpenEVSEManager,
+    ) -> None:
         super().__init__(coordinator)
         self.hass = hass
         self._config = config_entry
@@ -34,10 +49,11 @@ class OpenEVSESelect(CoordinatorEntity, SelectEntity):
         self._type = name
         self._attr_name = f"{config_entry.data[CONF_NAME]} {SELECT_TYPES[name][0]}"
         self._attr_unique_id = f"{self._attr_name}_{config_entry.entry_id}"
-        self._attr_current_option = self.coordinator.data[name]
+        self._attr_current_option = self.coordinator.data[self._type]
         self._attr_options = self.get_options()
-        self._command = SELECT_TYPES[name][3]
-        self._unit_of_measurement = SELECT_TYPES[name][1]
+        self._command = SELECT_TYPES[name][2]
+        self._manager = manager
+        self._entity_category = SELECT_TYPES[name][3]
 
     @property
     def device_info(self):
@@ -49,19 +65,26 @@ class OpenEVSESelect(CoordinatorEntity, SelectEntity):
         }
         return info
 
+    @property
+    def current_option(self) -> str | None:
+        """Return the selected entity option to represent the entity state."""
+        return str(self.coordinator.data[self._type])
+
     async def async_select_option(self, option: Any) -> None:
         """Change the selected option."""
-        host = self._config.data.get(CONF_HOST)
-        username = self._config.data.get(CONF_USERNAME)
-        password = self._config.data.get(CONF_PASSWORD)
-        charger = await self.hass.async_add_executor_job(
-            connect, host, username, password
-        )
+        charger = self._manager
         command = f"{self._command} {option}"
+        _LOGGER.debug("Command: %s", command)
         try:
-            await self.hass.async_add_executor_job(send_command, charger, command)
-        except (RequestException, ValueError, KeyError):
-            _LOGGER.warning("Could not set status for %s", self._attr_name)
+            await send_command(charger, command)
+        except (ValueError, KeyError) as err:
+            _LOGGER.warning(
+                "Could not set status for %s error: %s", self._attr_name, err
+            )
+        except InvalidValue:
+            _LOGGER.error(f"Value {option} invalid for command {self._command}.")
+        except CommandFailed:
+            _LOGGER.error(f"Command {self._command} failed.")
 
     @property
     def available(self) -> bool:
@@ -69,13 +92,17 @@ class OpenEVSESelect(CoordinatorEntity, SelectEntity):
         return self.coordinator.last_update_success
 
     @property
-    def unit_of_measurement(self) -> Optional[str]:
-        """Return the unit of measurement of this sensor."""
-        return self._unit_of_measurement
+    def entity_category(self) -> str | None:
+        """Return the category of the entity, if any."""
+        return self._entity_category
 
-    def get_options(self) -> list:
+    def get_options(self) -> list[str]:
+        """Return a set of selectable options."""
         if self._type == "current_capacity":
             min = self.coordinator.data["min_amps"]
             max = self.coordinator.data["max_amps"]
-            return list([item for item in range(min, max + 1)])
-        return SELECT_TYPES[self._type][2]
+            _LOGGER.debug(
+                "Max Amps: %s", list([str(item) for item in range(min, max + 1)])
+            )
+            return list([str(item) for item in range(min, max + 1)])
+        return SELECT_TYPES[self._type][1]
