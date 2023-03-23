2a53abd535cc099cc3c6069db5aec3e7729acbe1
firstof9@gmail.com <firstof9@gmail.com>
2023-03-23 09:13:18 -0700

    fix lookup by device_id

--- custom_components/openevse/services.py
+++ custom_components/openevse/services.py
@@ -46,7 +46,7 @@ class OpenEVSEServices:
             self._set_override,
             schema=vol.Schema(
                 {
-                    vol.Required(ATTR_DEVICE_ID): vol.Coerce(str),
+                    vol.Required(ATTR_DEVICE_ID): vol.Coerce(list),
                     vol.Optional(ATTR_STATE): vol.Coerce(str),
                     vol.Optional(ATTR_CHARGE_CURRENT): vol.All(
                         vol.Coerce(int), vol.Range(min=1, max=48)
@@ -71,7 +71,7 @@ class OpenEVSEServices:
             self._clear_override,
             schema=vol.Schema(
                 {
-                    vol.Required(ATTR_DEVICE_ID): vol.Coerce(str),
+                    vol.Required(ATTR_DEVICE_ID): vol.Coerce(list),
                 }
             ),
         )
@@ -80,80 +80,74 @@ class OpenEVSEServices:
     async def _set_override(self, service: ServiceCall) -> None:
         """Set the override."""
         data = service.data
-        if ATTR_DEVICE_ID in data:
-            device_id = data[ATTR_DEVICE_ID]
+        for device in data[ATTR_DEVICE_ID]:
+            device_id = device
             _LOGGER.debug("Device ID: %s", device_id)
-        else:
-            raise ValueError
 
-        dev_reg = dr.async_get(self.hass)
-        _LOGGER.debug("DR: %s", dir(dev_reg))
-        device_entry = dev_reg.async_get(device_id)
-        _LOGGER.debug("Device_entry: %s", device_entry)
+            dev_reg = dr.async_get(self.hass)
+            device_entry = dev_reg.async_get(device_id)
+            _LOGGER.debug("Device_entry: %s", device_entry)
 
-        if not device_entry:
-            raise ValueError(f"Device ID {device_id} is not valid")
+            if not device_entry:
+                raise ValueError(f"Device ID {device_id} is not valid")
 
-        config_id = device_entry.config_entries
-        _LOGGER.debug("Config ID: %s", config_id)
-        _LOGGER.debug("DEBUG: %s", self.hass.data[DOMAIN][config_id][MANAGER])
-        manager = self.hass.data[DOMAIN][config_id][MANAGER]
-        _LOGGER.debug("Manager: %s", self._manager)
+            config_id = list(device_entry.config_entries)[0]
+            _LOGGER.debug("Config ID: %s", config_id)
+            manager = self.hass.data[DOMAIN][config_id][MANAGER]
 
-        if ATTR_STATE in data:
-            state = data[ATTR_STATE]
-        else:
-            state = None
-        if ATTR_CHARGE_CURRENT in data:
-            charge_current = data[ATTR_CHARGE_CURRENT]
-        else:
-            charge_current = None
-        if ATTR_MAX_CURRENT in data:
-            max_current = data[ATTR_MAX_CURRENT]
-        else:
-            max_current = None
-        if ATTR_ENERGY_LIMIT in data:
-            energy_limit = data[ATTR_ENERGY_LIMIT]
-        else:
-            energy_limit = None
-        if ATTR_TIME_LIMIT in data:
-            time_limit = data[ATTR_TIME_LIMIT]
-        else:
-            time_limit = None
-        if ATTR_AUTO_RELEASE in data:
-            auto_release = data[ATTR_AUTO_RELEASE]
-        else:
-            auto_release = None
+            if ATTR_STATE in data:
+                state = data[ATTR_STATE]
+            else:
+                state = None
+            if ATTR_CHARGE_CURRENT in data:
+                charge_current = data[ATTR_CHARGE_CURRENT]
+            else:
+                charge_current = None
+            if ATTR_MAX_CURRENT in data:
+                max_current = data[ATTR_MAX_CURRENT]
+            else:
+                max_current = None
+            if ATTR_ENERGY_LIMIT in data:
+                energy_limit = data[ATTR_ENERGY_LIMIT]
+            else:
+                energy_limit = None
+            if ATTR_TIME_LIMIT in data:
+                time_limit = data[ATTR_TIME_LIMIT]
+            else:
+                time_limit = None
+            if ATTR_AUTO_RELEASE in data:
+                auto_release = data[ATTR_AUTO_RELEASE]
+            else:
+                auto_release = None
 
-        response = await manager.set_override(
-            state=state,
-            charge_current=charge_current,
-            max_current=max_current,
-            energy_limit=energy_limit,
-            time_limit=time_limit,
-            auto_release=auto_release,
-        )
-        _LOGGER.debug("Set Override response: %s", response)
+            response = await manager.set_override(
+                state=state,
+                charge_current=charge_current,
+                max_current=max_current,
+                energy_limit=energy_limit,
+                time_limit=time_limit,
+                auto_release=auto_release,
+            )
+            _LOGGER.debug("Set Override response: %s", response)
 
     async def _clear_override(self, service: ServiceCall) -> None:
         """Clear the manual override."""
         data = service.data
-        if ATTR_DEVICE_ID in data:
-            device_id = data[ATTR_DEVICE_ID]
+        _LOGGER.debug("Data: %s", data)
+        for device in data[ATTR_DEVICE_ID]:
+            device_id = device
             _LOGGER.debug("Device ID: %s", device_id)
-        else:
-            raise ValueError
         
-        dev_reg = dr.async_get(self.hass)
-        device_entry = dev_reg.async_get(device_id)
-        _LOGGER.debug("DR: %s", dir(dev_reg))
-        _LOGGER.debug("Device_entry: %s", device_entry)
+            dev_reg = dr.async_get(self.hass)
+            device_entry = dev_reg.async_get(device_id)
+            _LOGGER.debug("Device_entry: %s", device_entry)
 
-        if not device_entry:
-            raise ValueError(f"Device ID {device_id} is not valid")
+            if not device_entry:
+                raise ValueError(f"Device ID {device_id} is not valid")
 
-        config_id = device_entry.config_entries   
-        manager = self.hass.data[DOMAIN][config_id][MANAGER]
+            config_id = list(device_entry.config_entries)[0]
+            _LOGGER.debug("Config ID: %s Type: %s", config_id, type(config_id))
+            manager = self.hass.data[DOMAIN][config_id][MANAGER]
 
-        await manager.clear_override()
-        _LOGGER.debug("Override clear command sent.")
+            await manager.clear_override()
+            _LOGGER.debug("Override clear command sent.")
