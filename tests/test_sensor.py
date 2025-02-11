79e5c766d45f6f89ce964a761970a6ed82564822
firstof9@gmail.com <firstof9@gmail.com>
2025-02-11 16:09:37 -0700

    code cleanup

--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -1,5 +1,6 @@
 """Test openevse sensors."""
 
+import logging
 import json
 from datetime import timedelta
 from unittest.mock import patch
@@ -30,6 +31,7 @@ async def test_sensors(
     mock_ws_start,
     mock_aioclient,
     entity_registry: er.EntityRegistry,
+    caplog,
 ):
     """Test setup_entry."""
     entry = MockConfigEntry(
@@ -37,59 +39,63 @@ async def test_sensors(
         title=CHARGER_NAME,
         data=CONFIG_DATA,
     )
+    with caplog.at_level(logging.DEBUG):
+        entry.add_to_hass(hass)
+        assert await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
 
-    entry.add_to_hass(hass)
-    assert await hass.config_entries.async_setup(entry.entry_id)
-    await hass.async_block_till_done()
+        assert len(hass.states.async_entity_ids(BINARY_SENSOR_DOMAIN)) == 4
+        assert len(hass.states.async_entity_ids(SENSOR_DOMAIN)) == 22
+        assert len(hass.states.async_entity_ids(SWITCH_DOMAIN)) == 4
+        assert len(hass.states.async_entity_ids(SELECT_DOMAIN)) == 2
+        assert len(hass.states.async_entity_ids(NUMBER_DOMAIN)) == 1
+        entries = hass.config_entries.async_entries(DOMAIN)
+        assert len(entries) == 1
 
-    assert len(hass.states.async_entity_ids(BINARY_SENSOR_DOMAIN)) == 4
-    assert len(hass.states.async_entity_ids(SENSOR_DOMAIN)) == 22
-    assert len(hass.states.async_entity_ids(SWITCH_DOMAIN)) == 4
-    assert len(hass.states.async_entity_ids(SELECT_DOMAIN)) == 2
-    assert len(hass.states.async_entity_ids(NUMBER_DOMAIN)) == 1
-    entries = hass.config_entries.async_entries(DOMAIN)
-    assert len(entries) == 1
+        assert DOMAIN in hass.config.components
 
-    assert DOMAIN in hass.config.components
+        state = hass.states.get("sensor.openevse_wifi_firmware_version")
+        assert state
+        assert state.state == "v5.1.2"
+        state = hass.states.get("sensor.openevse_charge_time_elapsed")
+        assert state
+        assert state.state == "4.1"
+        state = hass.states.get("sensor.openevse_total_usage")
+        assert state
+        assert state.state == "64.582"
+        state = hass.states.get("sensor.openevse_max_current")
+        assert state
+        assert state.state == "48"
 
-    state = hass.states.get("sensor.openevse_wifi_firmware_version")
-    assert state
-    assert state.state == "v5.1.2"
-    state = hass.states.get("sensor.openevse_charge_time_elapsed")
-    assert state
-    assert state.state == "4.1"
-    state = hass.states.get("sensor.openevse_total_usage")
-    assert state
-    assert state.state == "64.582"
-    state = hass.states.get("sensor.openevse_max_current")
-    assert state
-    assert state.state == "48"
+        state = hass.states.get("sensor.openevse_override_state")
+        assert state
+        assert state.state == "auto"
 
-    state = hass.states.get("sensor.openevse_override_state")
-    assert state
-    assert state.state == "auto"
+        state = hass.states.get("sensor.openevse_charging_status")
+        assert state    
+        assert state.attributes.get("icon") == "mdi:sleep"
 
-    # enable disabled sensor
-    entity_id = "sensor.openevse_vehicle_charge_completion_time"
-    entity_entry = entity_registry.async_get(entity_id)
+        # enable disabled sensor
+        entity_id = "sensor.openevse_vehicle_charge_completion_time"
+        entity_entry = entity_registry.async_get(entity_id)
 
-    assert entity_entry
-    assert entity_entry.disabled
-    assert entity_entry.disabled_by is er.RegistryEntryDisabler.INTEGRATION
+        assert entity_entry
+        assert entity_entry.disabled
+        assert entity_entry.disabled_by is er.RegistryEntryDisabler.INTEGRATION
 
-    updated_entry = entity_registry.async_update_entity(
-        entity_entry.entity_id, disabled_by=None
-    )
-    assert updated_entry != entity_entry
-    assert updated_entry.disabled is False
+        updated_entry = entity_registry.async_update_entity(
+            entity_entry.entity_id, disabled_by=None
+        )
+        assert updated_entry != entity_entry
+        assert updated_entry.disabled is False
 
-    # reload the integration
-    assert await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
-    await hass.async_block_till_done()
+        # reload the integration
+        assert await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+        await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+        await hass.async_block_till_done()
 
-    state = hass.states.get("sensor.openevse_vehicle_charge_completion_time")
-    assert state
-    assert state.state == (dt_util.utcnow() + timedelta(seconds=18000)).isoformat(
-        timespec="seconds"
-    )
+        state = hass.states.get("sensor.openevse_vehicle_charge_completion_time")
+        assert state
+        assert state.state == (dt_util.utcnow() + timedelta(seconds=18000)).isoformat(
+            timespec="seconds"
+        )
